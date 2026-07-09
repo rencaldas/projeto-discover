a07f0b27d0933da86e6c5f17188308f3f199b8ad
Renato de Azevedo Caldas <renato.deacaldas@gmail.com>
2026-07-09 20:58:44 -0300

    Added files via upload
    initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+
+  //pegar a tag img
+  const img = document.querySelector("#profile img")
+
+  //substituir a imagem
+  if (html.classList.contains("light")) {
+    //se tiver light mode, adicionar a imagem light
+    img.setAttribute("src", "./assets/avatar-light.png")
+    img.setAttribute("alt", "Foto de Renato Caldas com fundo roxo e azul para perfil")
+  } else {
+    //se tiver sem light mode, manter a imagem original
+    img.setAttribute("src", "./assets/avatar.png")
+    img.setAttribute("alt", "Foto de Renato Caldas com fundo amarelo para perfil")
+  }
+}
